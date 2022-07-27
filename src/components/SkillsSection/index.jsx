d33e9864dff5e36cf9867a6c0ec047c9482d1a58
Luke Sutton <98839726+luke-sutton@users.noreply.github.com>
2022-07-27 21:10:57 +0100

    Video plays inline and text amendments

--- src/components/SkillsSection/index.jsx
+++ src/components/SkillsSection/index.jsx
@@ -14,15 +14,24 @@ const SkillsSection = () => {
     return (
         <SkillsContainer id="skills">
             <SkillsBg>
-                <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+                <VideoBg
+                    autoPlay
+                    playsInline
+                    loop
+                    muted
+                    src={Video}
+                    type="video/mp4"
+                />
             </SkillsBg>
             <SkillsContent>
                 <SkillsH1>Skills / Tech</SkillsH1>
                 <SkillsP>
-                    At work - I am currently imployed as a full time web developer using .Net, SQL, Umbraco and Vue.
+                    At work - I am currently employed as a full time web
+                    developer using .Net, SQL, Umbraco and Vue.
                 </SkillsP>
                 <SkillsP>
-                    At home - For my own projects I am currently enjoying the MERN stack (MongoDB, Express, React and Node).
+                    At home - For my own projects I am currently enjoying the
+                    MERN stack (MongoDB, Express, React and Node).
                 </SkillsP>
             </SkillsContent>
         </SkillsContainer>
